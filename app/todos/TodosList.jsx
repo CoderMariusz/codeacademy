3385513f7cb516b6c3fc0d446b59d5cd54b73c94
Mariusz Krawczyk <przyslony@gmail.com>
2023-01-12 23:40:48 +0000

    add log to function

--- app/todos/TodosList.jsx
+++ app/todos/TodosList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 function TodosList({ todos }) {
+  console.log(todos);
   return (
     <div className='w-full justify-items-center grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3'>
       {todos.map((todo) => (
