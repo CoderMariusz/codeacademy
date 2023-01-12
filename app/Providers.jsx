4966bcf463ec28707bf93a0205c176faeceebf5b
Mariusz Krawczyk <przyslony@gmail.com>
2023-01-12 22:05:47 +0000

    delete windows element from providers

--- app/Providers.jsx
+++ app/Providers.jsx
@@ -5,13 +5,13 @@ import { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
 export default function Providers({ children }) {
-  const storage = window.sessionStorage.getItem('user');
+  const storage = sessionStorage.getItem('user');
   const [user, setUser] = useState(storage ? JSON.parse(storage) : null);
 
   function login(user) {
     console.log('login');
     setUser(user);
-    window.sessionStorage.setItem('user', JSON.stringify(user));
+    sessionStorage.setItem('user', JSON.stringify(user));
   }
   function currentUser() {
     return user;
