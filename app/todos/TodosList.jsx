303a7bc5c3280ad1a3dc7c60e2f74cf2999e3b6f
Mariusz Krawczyk <przyslony@gmail.com>
2023-01-13 14:31:36 +0000

    change for todos to async function to see if it working

--- app/todos/TodosList.jsx
+++ app/todos/TodosList.jsx
@@ -13,17 +13,18 @@ function TodosList({ todos }) {
   console.log('second todos:', todos2);
   return (
     <div className='w-full justify-items-center grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3'>
-      {todos.map((todo) => (
-        <Card
-          key={todo.id}
-          title={todo.title}
-          description={todo.description}
-          priority={todo.priority}
-          time={todo.time}
-          date={todo.date}
-          user={todo.user}
-        />
-      ))}
+      {todos2 &&
+        todos2.map((todo) => (
+          <Card
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+            priority={todo.priority}
+            time={todo.time}
+            date={todo.date}
+            user={todo.user}
+          />
+        ))}
     </div>
   );
 }
