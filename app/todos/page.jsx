bb39c4d083d8dba805c725f177af6ae98b27f335
Mariusz Krawczyk <przyslony@gmail.com>
2023-01-13 15:42:51 +0000

    todos change for let

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -2,7 +2,8 @@ import { getTodos } from '../../firebase/firebase';
 import TodosList from './TodosList';
 
 async function TodosPage() {
-  const todos = await getTodos();
+  let todos = await getTodos();
+
   return (
     <div className='flex justify-center items-center'>
       <TodosList todos={todos} />
