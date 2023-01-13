175bbe6d44e8a61fb95d43febc56d85222a3875b
Mariusz Krawczyk <przyslony@gmail.com>
2023-01-13 15:02:35 +0000

    remove function from element

--- app/todos/TodosList.jsx
+++ app/todos/TodosList.jsx
@@ -1,20 +1,13 @@
 'use client';
-import React from 'react';
 import Card from './Card';
 import { getTodos } from '../../firebase/firebase';
 
 function TodosList({ todos }) {
-  console.log(todos);
-  async function getTodos2() {
-    const todos2 = await getTodos();
-    return todos2;
-  }
-  const todos2 = getTodos2();
-  console.log('second todos:', todos2);
+  console.log('todos:', todos);
   return (
     <div className='w-full justify-items-center grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3'>
-      {todos2 &&
-        todos2.map((todo) => (
+      {todos &&
+        todos.map((todo) => (
           <Card
             key={todo.id}
             title={todo.title}
